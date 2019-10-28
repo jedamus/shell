1d3729cf3d56aa83a6f570ce035b3cb1fe30b103
Leander Jedamus <ljedamus@web.de>
2019-10-28 15:43:36 +0100

    added several things to be removed

--- play_music.zsh
+++ play_music.zsh
@@ -2,12 +2,16 @@
 
 #echo $RANDOM
 
-tmpfile=/tmp/play_music.tmp
+tmpfile=/tmp/play_music.in.tmp
+tmpfile2=/tmp/play_music.out.tmp
 
 cd $HOME/Musik/mp3/stick/mp3
-echo -n "totem --play-pause " > $tmpfile
+#echo -n "totem --play-pause " > $tmpfile
+echo -n "" > $tmpfile
 #ls */*/*.mp3 | sed 's/ /\\ /g' | sed "s/'/\\\\'/g" | sed 's/-/\\-/g' | sed 's/[(]/\\(/g' | sed 's/[)]/\\)/g' | sed 's/$/ \\/g' >> $tmpfile
-ls */*/*.mp3 | sed "s/'/\\'/g" | sed 's/\([\(\) -&!]\)/\\\1/g' | sed 's/$/ \\/g' >> $tmpfile
-echo "" >> $tmpfile
-#FILES=`cat $tmpfile`
+ls */*/*.mp3 | sed "s/'/\\'/g" | sed 's/\([\(\) -\&\!]\)/\\\1/g' | sed 's/$/ \\/g' | sed 's/\\\//\//g' | sed 's/\\\././g' >> $tmpfile
+
+randomize_file $tmpfile $tmpfile2 $RANDOM
+
+#FILES=`cat $tmpfile2`
 #totem --play-pause $FILES
