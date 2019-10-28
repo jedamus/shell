3c2843e0838d13d2c157df9aaca5f2ac51f2284e
Leander Jedamus <ljedamus@web.de>
2019-10-28 16:05:03 +0100

    added third tmpfile

--- play_music.zsh
+++ play_music.zsh
@@ -3,15 +3,18 @@
 #echo $RANDOM
 
 tmpfile=/tmp/play_music.in.tmp
-tmpfile2=/tmp/play_music.out.tmp
+tmpfile2=/tmp/play_music.middle.tmp
+tmpfile3=/tmp/play_music.out.tmp
 
 cd $HOME/Musik/mp3/stick/mp3
-#echo -n "totem --play-pause " > $tmpfile
-echo -n "" > $tmpfile
 #ls */*/*.mp3 | sed 's/ /\\ /g' | sed "s/'/\\\\'/g" | sed 's/-/\\-/g' | sed 's/[(]/\\(/g' | sed 's/[)]/\\)/g' | sed 's/$/ \\/g' >> $tmpfile
 ls */*/*.mp3 | sed "s/'/\\'/g" | sed 's/\([\(\) -\&\!]\)/\\\1/g' | sed 's/$/ \\/g' | sed 's/\\\//\//g' | sed 's/\\\././g' >> $tmpfile
 
 randomize_file $tmpfile $tmpfile2 $RANDOM
+echo "cd $HOME/Musik/mp3/stick/mp3" > $tmpfile3
+echo -n "totem --play-pause " >> $tmpfile3
+cat $tmpfile2 >> $tmpfile3
+echo -n "" >> $tmpfile3
 
 #FILES=`cat $tmpfile2`
 #totem --play-pause $FILES
