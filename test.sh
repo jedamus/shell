444f49d5931571233b6a12a9549211553233c61f
Leander Jedamus <ljedamus@web.de>
2019-10-29 18:52:37 +0100

    moved from bash back to zsh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# erzeugt Dienstag, 29. Oktober 2019 18:38 (C) 2019 von Leander Jedamus
+# modifiziert Dienstag, 29. Oktober 2019 18:51 von Leander Jedamus
+
+set -x
+
+mp3_what="*.txt"
+
+ls ${~mp3_what}
+
+# vim:ai sw=2
+
