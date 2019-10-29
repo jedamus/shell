d3f66c7289b287f2257651900b39766c25606a04
Leander Jedamus <ljedamus@web.de>
2019-10-29 13:43:01 +0100

    changed to run tmpfile3

--- play_music_mp3.zsh
+++ play_music_mp3.zsh
@@ -4,19 +4,22 @@
 
 mp3_path=$HOME/Musik/mp3/stick/mp3
 
-tmpfile=/tmp/play_music.in.tmp
-tmpfile2=/tmp/play_music.middle.tmp
-tmpfile3=/tmp/play_music.out.tmp
+tmpfile=/tmp/play_music_mp3.in.tmp
+tmpfile2=/tmp/play_music_mp3.middle.tmp
+tmpfile3=/tmp/play_music_mp3.out.tmp
 
 cd $mp3_path
 #ls */*/*.mp3 | sed 's/ /\\ /g' | sed "s/'/\\\\'/g" | sed 's/-/\\-/g' | sed 's/[(]/\\(/g' | sed 's/[)]/\\)/g' | sed 's/$/ \\/g' >> $tmpfile
 ls */*/*.mp3 | sed "s/'/\\'/g" | sed 's/\([\(\) -\&]\)/\\\1/g' | sed 's/$/ \\/g' | sed 's/\\\//\//g' | sed 's/\\\././g' > $tmpfile
 
 randomize_file $tmpfile $tmpfile2 $RANDOM
-echo "cd $mp3_path" > $tmpfile3
+echo "'!/bin/sh" > $tmpfile3
+echo "cd $mp3_path" >> $tmpfile3
 echo -n "totem --play-pause " >> $tmpfile3
 cat $tmpfile2 | sed 's/^!//' >> $tmpfile3
 echo -n "" >> $tmpfile3
+chmod a+x $tmpfile3
+$tmpfile3
 
 #FILES=`cat $tmpfile2`
 #totem --play-pause $FILES
