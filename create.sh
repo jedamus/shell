95a4ee3259448e9598064edd0ea7ec7f806f750e
Leander Jedamus <ljedamus@web.de>
2020-04-27 08:06:40 +0200

    added create.sh and install.sh

---
+++ create.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# erzeugt Samstag, 21. März 2020 13:53 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 27. April 2020 07:54 von Leander Jedamus
+# modifiziert Samstag, 25. April 2020 10:47 von Leander Jedamus
+# modifiziert Mittwoch, 22. April 2020 18:52 von Leander Jedamus
+# modifiziert Montag, 06. April 2020 18:22 von Leander Jedamus
+# modifiziert Dienstag, 31. März 2020 19:24 von Leander Jedamus
+# modifiziert Donnerstag, 26. März 2020 17:06 von Leander Jedamus
+# modifiziert Samstag, 21. März 2020 17:17 von Leander Jedamus
+
+stick=/media/leander/INTENSO
+
+dir=$stick/von_leander
+dir=$HOME/Install_My_Repos
+
+cd
+tar cvfz $dir/vim.tar.gz ./vim
+tar cvfz $dir/zsh.tar.gz ./zsh ./todo.txt
+
+bin=./bin
+
+tar cvfz $dir/bin.tar.gz $bin/nocaps.sh $bin/gitinit.sh $bin/rep.sh $bin/grep_in_dpkg.sh $bin/chmodchown.modify_me $bin/mount_iso.sh $bin/man2pdf.sh $bin/man2ps.sh $bin/update.sh $bin/notebook.sh
+
+
+tar cvfz $dir/dotfiles.tar.gz .env .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
+
+projekte=./Projekte
+perl=$projekte/perl
+python=$projekte/python
+
+tar cvfz $dir/Projekte.tar.gz $perl/myperl $projekte/latex $python/download-sortierer $python/active-print $python/matrizen $python/notebook $python/qiskit $python/pyquil $python/parallel $python/ipc $python/pylab_matplotlib_pandas $python/numpy_scipy_matplotlib $projekte/deb/randomizefile $projekte/automake/am_randomize_file $projekte/make
+
+# vim:ai sw=2
+
---
+++ install.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env sh
+
+# erzeugt Samstag, 21. März 2020 11:36 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 27. April 2020 07:52 von Leander Jedamus
+# modifiziert Donnerstag, 26. März 2020 13:33 von Leander Jedamus
+# modifiziert Sonntag, 22. März 2020 15:06 von Leander Jedamus
+# modifiziert Samstag, 21. März 2020 15:19 von Leander Jedamus
+
+# modify this to reflect your printers!
+printers="laserjet duplex"
+
+my_path=`pwd`
+bin=$HOME/bin
+
+modify_desktop_file()
+{
+  echo "installing $2"
+  cat $1 | sed "s/USER/$USER/g" | sed "s/PRINTER/$3/g" > $2
+};# modify_desktop_file
+
+echo "After entering your password, write the following to change the shell:"
+echo "\"/bin/zsh\":"
+chsh
+
+cd
+tar xvfz $my_path/dotfiles.tar.gz
+tar xvfz $my_path/bin.tar.gz
+
+cd $bin
+ln -sf a2pl.pl a2ps
+
+CH=chmodchown
+cat ${CH}.modify_me | sed "s/__USER__/${USER}/" > ${CH}.sh
+rm ${CH}.modify_me
+chmod +x ${CH}.sh
+
+cd
+tar xvfz $my_path/zsh.tar.gz
+tar xvfz $my_path/vim.tar.gz
+tar xvfz $my_path/Projekte.tar.gz
+
+# install zsh
+cp -p zsh/zshprompt.py $bin
+cp -p zsh/.zshrc zsh/.env $HOME
+
+# install vim
+cp -p vim/.exrc $HOME
+cp -p vim/.vimrc3 $HOME/.vimrc
+
+# install Projekte
+my_print=$HOME/print
+projekte=$HOME/Projekte
+myperl=$projekte/perl/myperl
+python=$projekte/python
+zlogin=$HOME/.zlogin
+
+cd $myperl/a2ps
+cp -Rvp a2ps.pl translate $bin
+
+cd $myperl/myconf
+cp -Rvp myconf.pl $bin
+
+cd $myperl/mycopy
+cp -Rvp mycopy.pl translate $bin
+
+cd $myperl/translate
+cp -Rvp translate.pl $bin
+
+autostart=$HOME/.config/autostart
+mkdir -p $autostart
+
+cd $python/download-sortierer
+cp -Rvp download-sortierer.py translate $bin
+modify_desktop_file download-sortierer.desktop $autostart/download-sortierer.desktop
+
+echo "All PDF files, which are COPIED into these subdrectories of ~/print,\nare printed on the corresponding printer and then DELETED!\n\nAlle PDF-Dateien, die in Unterverzeichnisse von ~/print/ KOPIERT werden,\nwerden auf dem ensprechenden Drucker ausgedruckt und anschließend GELÖSCHT!" > $my_print/README.IMPORTANT\!
+
+echo "#echo \"ich bin $zlogin\"\n" > $zlogin
+
+# für jeden Druckereintrag ein Verzeichnis anlegen und in .zlogin eintragen:
+for i in $printers; do
+  echo "creating dir for printer $i"
+  mkdir -p $my_print/$i
+  echo "echo -n \"active-print.py -P $i: \"" >> $zlogin
+  echo "~/bin/active-print.py -P $i 2> /dev/null &" >> $zlogin
+done
+
+cd $python/active-print
+cp -Rvp active-print.py translate $bin
+for i in $printers; do
+  modify_desktop_file active-print.desktop $autostart/active-print-$i.desktop $i
+done
+
+echo ""
+echo "Now log off completely and re-log in!"
+
+# vim:ai sw=2
+
