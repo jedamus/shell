f5b419e5d3dee302faec1c373a65c5c0691884e5
Leander Jedamus <ljedamus@web.de>
2020-04-27 08:09:17 +0200

    minor changes

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 21. März 2020 13:53 (C) 2020 von Leander Jedamus
-# modifiziert Montag, 27. April 2020 07:54 von Leander Jedamus
+# modifiziert Montag, 27. April 2020 08:08 von Leander Jedamus
 # modifiziert Samstag, 25. April 2020 10:47 von Leander Jedamus
 # modifiziert Mittwoch, 22. April 2020 18:52 von Leander Jedamus
 # modifiziert Montag, 06. April 2020 18:22 von Leander Jedamus
@@ -15,6 +15,8 @@ dir=$stick/von_leander
 dir=$HOME/Install_My_Repos
 
 cd
+tar cvfz $dir/dotfiles.tar.gz .env .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
+
 tar cvfz $dir/vim.tar.gz ./vim
 tar cvfz $dir/zsh.tar.gz ./zsh ./todo.txt
 
@@ -22,14 +24,11 @@ bin=./bin
 
 tar cvfz $dir/bin.tar.gz $bin/nocaps.sh $bin/gitinit.sh $bin/rep.sh $bin/grep_in_dpkg.sh $bin/chmodchown.modify_me $bin/mount_iso.sh $bin/man2pdf.sh $bin/man2ps.sh $bin/update.sh $bin/notebook.sh
 
-
-tar cvfz $dir/dotfiles.tar.gz .env .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
-
 projekte=./Projekte
 perl=$projekte/perl
 python=$projekte/python
 
-tar cvfz $dir/Projekte.tar.gz $perl/myperl $projekte/latex $python/download-sortierer $python/active-print $python/matrizen $python/notebook $python/qiskit $python/pyquil $python/parallel $python/ipc $python/pylab_matplotlib_pandas $python/numpy_scipy_matplotlib $projekte/deb/randomizefile $projekte/automake/am_randomize_file $projekte/make
+tar cvfz $dir/Projekte.tar.gz $perl/myperl $projekte/latex $python/download-sortierer $python/active-print $python/matrizen $python/notebook $python/qiskit $python/pyquil $python/parallel $python/ipc $python/pylab_matplotlib_pandas $python/numpy_scipy_matplotlib $projekte/deb/randomizefile $projekte/automake/am_randomize_file $projekte/make $projekte/shell
 
 # vim:ai sw=2
 
