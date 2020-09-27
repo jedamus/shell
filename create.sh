ec0f90da6e7a89704e0404eaddf3dcb096599336
Leander Jedamus <ljedamus@web.de>
2020-09-27 02:03:55 +0200

    minor changes

--- create.sh
+++ create.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 21. März 2020 13:53 (C) 2020 von Leander Jedamus
+# modifiziert Sonntag, 27. September 2020 01:17 von Leander Jedamus
 # modifiziert Donnerstag, 24. September 2020 11:28 von Leander Jedamus
 # modifiziert Montag, 07. September 2020 13:03 von Leander Jedamus
 # modifiziert Montag, 27. April 2020 09:29 von Leander Jedamus
@@ -17,9 +18,9 @@ dir=$stick/von_leander
 dir=$HOME/Install_My_Repos
 
 cd
-tar cvfz $dir/dotfiles.tar.gz .env .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
+tar cvfz $dir/dotfiles.tar.gz .myenv .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
 
-tar cvfz $dir/vim.tar.gz ./vim
+tar cvfz $dir/vim.tar.gz ./.vim
 tar cvfz $dir/zsh.tar.gz ./zsh ./todo.txt
 
 bin=./bin
