37f2dc933b99151db0f309504574e717e3935be7
Leander Jedamus <ljedamus@web.de>
2020-12-03 18:55:38 +0100

    added new create.sh and install.sh

--- create.sh
+++ create.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 21. März 2020 13:53 (C) 2020 von Leander Jedamus
+# modifiziert Donnerstag, 03. Dezember 2020 17:20 von Leander Jedamus
+# modifiziert Mittwoch, 02. Dezember 2020 13:55 von Leander Jedamus
+# modifiziert Sonntag, 22. November 2020 09:57 von Leander Jedamus
+# modifiziert Donnerstag, 19. November 2020 10:04 von Leander Jedamus
+# modifiziert Montag, 26. Oktober 2020 10:13 von Leander Jedamus
+# modifiziert Sonntag, 04. Oktober 2020 07:58 von Leander Jedamus
+# modifiziert Samstag, 03. Oktober 2020 13:30 von Leander Jedamus
+# modifiziert Freitag, 02. Oktober 2020 22:33 von Leander Jedamus
 # modifiziert Sonntag, 27. September 2020 01:17 von Leander Jedamus
 # modifiziert Donnerstag, 24. September 2020 11:28 von Leander Jedamus
 # modifiziert Montag, 07. September 2020 13:03 von Leander Jedamus
@@ -18,20 +26,20 @@ dir=$stick/von_leander
 dir=$HOME/Install_My_Repos
 
 cd
-tar cvfz $dir/dotfiles.tar.gz .myenv .exrc .vimrc .zshrc .cshrc .bashrc .login .zlogin .zlogout .zprofile .zsh_history .bash_logout .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator
+tar cvfz $dir/dotfiles.tar.gz .shenv .exrc .cshrc .tcshrc .login .logout .history .cshdirs .bash_profile .bashrc .bash_logout .bash_history .mkshrc .profile .zshenv .zprofile .zshrc .zlogin .zlogout .zsh_history .gitconfig .tmux.conf .condarc .condarc.example .forest_config .byobu .tmuxinator .tmux
 
 tar cvfz $dir/vim.tar.gz ./.vim
-tar cvfz $dir/zsh.tar.gz ./zsh ./todo.txt
+tar cvfz $dir/zsh.tar.gz ./.zsh ./todo.txt
 
 bin=./bin
 
-tar cvfz $dir/bin.tar.gz $bin/nocaps.sh $bin/gitinit.sh $bin/rep.sh $bin/evince.sh $bin/ogg.sh $bin/grep_in_dpkg.sh $bin/chmodchown.modify_me $bin/mount_iso.sh $bin/man2pdf.sh $bin/man2ps.sh $bin/update.sh $bin/notebook.sh
+tar cvfz $dir/bin.tar.gz $bin/nocaps.sh $bin/gitinit.sh $bin/rep.sh $bin/evince.sh $bin/ogg.sh $bin/my_options.sh $bin/grep_in_dpkg.sh $bin/chmodchown.modify_me $bin/mount_iso.sh $bin/man2pdf.sh $bin/man2ps.sh $bin/update.sh $bin/notebook.sh $bin/conda_init.sh
 
 projekte=./Projekte
 perl=$projekte/perl
 python=$projekte/python
 
-tar cvfz $dir/Projekte.tar.gz $perl/myperl $projekte/latex $python/download-sortierer $python/active-print $python/matrizen $python/notebook $python/qiskit $python/pyquil $python/parallel $python/ipc $python/pylab_matplotlib_pandas $python/numpy_scipy_matplotlib $projekte/deb/randomizefile $projekte/automake/am_randomize_file $projekte/make $projekte/shell $projekte/tmux
+tar cvfz $dir/Projekte.tar.gz $perl/myperl $projekte/latex $python/download-sortierer $python/active-print $python/matrizen $python/beispiele $python/notebook $python/qiskit $python/pyquil $python/parallel $python/ipc $python/pylab_matplotlib_pandas $python/numpy_scipy_matplotlib $projekte/deb/randomizefile $projekte/automake/am_randomize_file $projekte/make $projekte/shell $projekte/tmux
 
 # vim:ai sw=2
 
--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 21. März 2020 11:36 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 01. Dezember 2020 08:59 von Leander Jedamus
+# modifiziert Donnerstag, 19. November 2020 09:30 von Leander Jedamus
+# modifiziert Montag, 26. Oktober 2020 10:32 von Leander Jedamus
+# modifiziert Sonntag, 04. Oktober 2020 08:03 von Leander Jedamus
+# modifiziert Samstag, 03. Oktober 2020 04:00 von Leander Jedamus
+# modifiziert Sonntag, 27. September 2020 08:05 von Leander Jedamus
 # modifiziert Montag, 27. April 2020 07:52 von Leander Jedamus
 # modifiziert Donnerstag, 26. März 2020 13:33 von Leander Jedamus
 # modifiziert Sonntag, 22. März 2020 15:06 von Leander Jedamus
@@ -27,21 +33,22 @@ tar xvfz $my_path/dotfiles.tar.gz
 tar xvfz $my_path/bin.tar.gz
 
 cd $bin
-ln -sf a2pl.pl a2ps
+ln -sf a2ps.pl a2ps
 
 CH=chmodchown
 cat ${CH}.modify_me | sed "s/__USER__/${USER}/" > ${CH}.sh
-rm ${CH}.modify_me
+rm -f ${CH}.modify_me
 chmod +x ${CH}.sh
 
 cd
 tar xvfz $my_path/zsh.tar.gz
 tar xvfz $my_path/vim.tar.gz
 tar xvfz $my_path/Projekte.tar.gz
+ln -sf .vim/vimrc3 .vimrc
 
 # install zsh
-cp -p zsh/zshprompt.py $bin
-cp -p zsh/.zshrc zsh/.env $HOME
+cp -p .zsh/zshprompt.py $bin
+cp -p .zsh/.zshrc .zsh/.myenv $HOME
 
 # install vim
 cp -p vim/.exrc $HOME
@@ -55,13 +62,13 @@ python=$projekte/python
 zlogin=$HOME/.zlogin
 
 cd $myperl/a2ps
-cp -Rvp a2ps.pl translate $bin
+cp -Rvp a2ps.pl locale $bin
 
 cd $myperl/myconf
 cp -Rvp myconf.pl $bin
 
 cd $myperl/mycopy
-cp -Rvp mycopy.pl translate $bin
+cp -Rvp mycopy.pl locale $bin
 
 cd $myperl/translate
 cp -Rvp translate.pl $bin
@@ -70,7 +77,7 @@ autostart=$HOME/.config/autostart
 mkdir -p $autostart
 
 cd $python/download-sortierer
-cp -Rvp download-sortierer.py translate $bin
+cp -Rvp download-sortierer.py locale $bin
 modify_desktop_file download-sortierer.desktop $autostart/download-sortierer.desktop
 
 echo "All PDF files, which are COPIED into these subdrectories of ~/print,\nare printed on the corresponding printer and then DELETED!\n\nAlle PDF-Dateien, die in Unterverzeichnisse von ~/print/ KOPIERT werden,\nwerden auf dem ensprechenden Drucker ausgedruckt und anschließend GELÖSCHT!" > $my_print/README.IMPORTANT\!
@@ -86,7 +93,7 @@ for i in $printers; do
 done
 
 cd $python/active-print
-cp -Rvp active-print.py translate $bin
+cp -Rvp active-print.py locale $bin
 for i in $printers; do
   modify_desktop_file active-print.desktop $autostart/active-print-$i.desktop $i
 done
