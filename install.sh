27e6ed8bc73b2f4811a4a4e489ce156d7c574f88
Leander Jedamus <ljedamus@web.de>
2020-12-14 18:32:30 +0100

    minor changes

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 18:19 (C) 2020 von Leander Jedamus
-# modifiziert Montag, 14. Dezember 2020 18:28 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 18:32 von Leander Jedamus
 
 cd files
 cp -vp *.sh *.zsh *.modify_me $HOME/bin
+echo ""
 
 cd $HOME/bin
 CH=chmodchown
@@ -13,9 +14,6 @@ cat ${CH}.modify_me | sed "s/__USER__/${USER}/" > ${CH}.sh
 echo "removing ${CH}.modify_me"
 rm -f ${CH}.modify_me
 chmod +x ${CH}.sh
-echo ""
-
-
 
 # vim:ai sw=2
 
