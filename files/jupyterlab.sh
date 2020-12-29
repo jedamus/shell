dae6d0f5b744c042cda6ff5cdfe2da79f768d965
Leander Jedamus <ljedamus@web.de>
2020-12-29 15:01:07 +0100

    changed to use jupyter-lab

--- files/jupyterlab.sh
+++ files/jupyterlab.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 # erzeugt Sonntag, 22. November 2020 09:43 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 29. Dezember 2020 14:30 von Leander Jedamus
 # modifiziert Sonntag, 22. November 2020 09:43 von Leander Jedamus
+
+jupyter-lab --browser="firefox"
 
 # vim:ai sw=2
 
