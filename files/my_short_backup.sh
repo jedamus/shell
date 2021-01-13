3f4a76d8793b6dd6359375f862cba7fef4a83644
Leander Jedamus <ljedamus@web.de>
2021-01-13 05:00:13 +0100

    minor changes

--- files/my_short_backup.sh
+++ files/my_short_backup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 28. März 2020 10:39 (C) 2020 von Leander Jedamus
-# modifiziert Mittwoch, 13. Januar 2021 04:55 von Leander Jedamus
+# modifiziert Mittwoch, 13. Januar 2021 04:59 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 14:35 von Leander Jedamus
 # modifiziert Donnerstag, 24. Dezember 2020 14:21 von Leander Jedamus
 # modifiziert Sonntag, 27. September 2020 05:47 von Leander Jedamus
 # modifiziert Samstag, 28. März 2020 10:42 von Leander Jedamus
 
-dir="/media/leander/INTENSO2"
+dir="/media/$USER/INTENSO2"
 counter=$HOME/bin/my_short_backup_nr
 
 mkdir -p $dir
