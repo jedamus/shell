f5611c36ead4880180e6f8521aa196e1a8ad653f
Leander Jedamus <ljedamus@web.de>
2021-01-15 11:43:05 +0100

    updated install.sh

--- install_my_repos/install.sh
+++ install_my_repos/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 21. März 2020 11:36 (C) 2020 von Leander Jedamus
+# modifiziert Freitag, 15. Januar 2021 11:34 von Leander Jedamus
+# modifiziert Mittwoch, 13. Januar 2021 06:59 von Leander Jedamus
 # modifiziert Freitag, 04. Dezember 2020 13:32 von Leander Jedamus
 # modifiziert Donnerstag, 03. Dezember 2020 19:39 von Leander Jedamus
 # modifiziert Dienstag, 01. Dezember 2020 08:59 von Leander Jedamus
@@ -15,22 +17,73 @@
 # modifiziert Samstag, 21. März 2020 15:19 von Leander Jedamus
 
 # modify this to reflect your printers!
-printers="laserjet duplex"
+printers_="laserjet duplex"
+
+name_="Test User"
+email_="testuser@testuser.org"
+www_="http://www.testuser.org/"
+package_="org.testuser"
 
 my_path=`pwd`
 bin=$HOME/bin
 
-modify_desktop_file()
-{
-  echo "installing $2"
-  cat $1 | sed "s/USER/$USER/g" | sed "s/PRINTER/$3/g" > $2
-};# modify_desktop_file
+target=`uname -s`
 
-echo "After entering your password, write the following to change the shell:"
-echo "\"/bin/zsh\":"
-chsh
+case "$target" in
+  Darwin) export machtype="MacOS" ;;
+  Linux)  export machtype="Linux" ;;
+  *)      export machtype="unknown" ;;
+esac
 
-cd
+target=`uname -v`
+
+case "$target" in
+  *Ubuntu*) export machsystem="Ubuntu" ;;
+  *Darwin*) export machsystem="MacOS" ;;
+  *)        export machsystem="unknown" ;;
+esac
+
+echo "Type is $machtype"
+echo "System is $machsystem"
+
+# debug-mode:
+## export machtype="MacOS"
+## export machsystem="MacOS"
+
+if [ -z "$1" ];then
+  name="$name_"
+  email="$email_"
+  www="$www_"
+  package="$package_"
+  printers="$printers_"
+else
+  read -p "Enter your printer name(s) delimited by space: " printers
+  echo "setting printers to \"$printers\""
+  echo ""
+
+  read -p "What is your name ($name_): " name
+  echo "setting your name to \"$name\""
+  echo ""
+
+  read -p "What is your email-address ($email_): " email
+  echo "setting your email-address to \"$email\""
+  echo ""
+
+  read -p "What is your WWW-address ($www_): " www
+  echo "setting your www-address to \"$www\""
+  echo ""
+
+  read -p "What is your Package-string ($package_): " package
+  echo "setting your package-string to \"$package\""
+  echo ""
+
+  echo "After entering your password, write the following to change the shell:"
+  echo "\"/bin/zsh\":"
+
+  chsh
+fi
+
+cd $HOME
 tar xvfz $my_path/dotfiles.tar.gz
 tar xvfz $my_path/bin.tar.gz
 
@@ -42,65 +95,67 @@ cat ${CH}.modify_me | sed "s/__USER__/${USER}/" > ${CH}.sh
 rm -f ${CH}.modify_me
 chmod +x ${CH}.sh
 
-cd
+cd $HOME
 tar xvfz $my_path/zsh.tar.gz
 tar xvfz $my_path/vim.tar.gz
 tar xvfz $my_path/Projekte.tar.gz
-ln -sf .vim/vimrc3 .vimrc
 
 # install zsh
-cp -p .zsh/zshprompt.py $bin
-cp -p .zsh/.zshrc .zsh/.myenv $HOME
+cd $HOME/.zsh
+sh ./install.sh
 
 # install vim
-cp -p .vim/.exrc $HOME
-#cp -p .vim/.vimrc3 $HOME/.vimrc
+cd $HOME/.vim
+
+if [ $machtype = "Linux" ]; then
+  sh ./install3.sh "$email" "$www" "$package"
+fi
+if [ $machtype = "MacOS" ]; then
+  sh ./install2.sh "$email" "$www" "$package"
+fi
 
 # install Projekte
 my_print=$HOME/print
 projekte=$HOME/Projekte
 myperl=$projekte/perl/myperl
 python=$projekte/python
-zlogin=$HOME/.zlogin
+dotfiles=$projekte/dotfiles
 
-cd $myperl/a2ps
-cp -Rvp a2ps.pl locale $bin
-
-cd $myperl/myconf
-cp -Rvp myconf.pl $bin
-
-cd $myperl/mycopy
-cp -Rvp mycopy.pl locale $bin
-
-cd $myperl/translate
-cp -Rvp translate.pl $bin
-
-autostart=$HOME/.config/autostart
-mkdir -p $autostart
-
-cd $python/download-sortierer
-cp -Rvp download-sortierer.py locale $bin
-modify_desktop_file download-sortierer.desktop $autostart/download-sortierer.desktop
-
-echo "All PDF files, which are COPIED into these subdrectories of ~/print,\nare printed on the corresponding printer and then DELETED!\n\nAlle PDF-Dateien, die in Unterverzeichnisse von ~/print/ KOPIERT werden,\nwerden auf dem ensprechenden Drucker ausgedruckt und anschließend GELÖSCHT!" > $my_print/README.IMPORTANT\!
-
-echo "#echo \"ich bin $zlogin\"\n" > $zlogin
-
-# für jeden Druckereintrag ein Verzeichnis anlegen und in .zlogin eintragen:
-for i in $printers; do
-  echo "creating dir for printer $i"
-  mkdir -p $my_print/$i
-  echo "echo -n \"active-print.py -P $i: \"" >> $zlogin
-  echo "~/bin/active-print.py -P $i 2> /dev/null &" >> $zlogin
-done
-
-cd $python/active-print
-cp -Rvp active-print.py locale $bin
-for i in $printers; do
-  modify_desktop_file active-print.desktop $autostart/active-print-$i.desktop $i
-done
-
+cd $dotfiles
+sh ./install.sh
 echo ""
+
+cd $projekte/git
+sh ./install.sh "$name" "$email"
+
+if [ $machtype = "Linux" ]; then
+  sh ./install_linux.sh
+fi
+if [ $machtype = "MacOS" ]; then
+  sh ./install_macos.sh
+fi
+
+cd $myperl
+sh ./install.sh
+echo ""
+
+cd $projekte/shell
+sh ./install.sh
+echo ""
+
+cd $projekte/tmux
+sh ./install.sh
+echo ""
+
+if [ $machtype = "Linux" ]; then
+  cd $python/download-sortierer
+  sh ./install.sh
+  echo ""
+  cd $python/active-print
+  sh ./install.sh $printers
+  echo ""
+fi
+
 echo "Now log off completely and re-log in!"
 
 # vim:ai sw=2
