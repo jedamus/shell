2d8383fa3f384b8a0944531b018a1c4252e09661
Leander Jedamus <ljedamus@web.de>
2022-04-03 00:56:08 +0200

    more output for create_private_key_list.sh

--- files/create_private_key_list.sh
+++ files/create_private_key_list.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Freitag, 18. März 2022 05:41 (C) 2022 von Leander Jedamus
+# modifiziert Samstag, 02. April 2022 23:00 von Leander Jedamus
 # modifiziert Freitag, 18. März 2022 05:56 von Leander Jedamus
 
 #set -x
@@ -8,20 +9,35 @@
 pfad=$HOME/.ssh
 private_key_list=$pfad/private_key_list
 
-mkdir -p $pfad
+if [ ! -d $pfad ]; then
+  mkdir -p $pfad
+  echo "created .ssh in your home directory."
+  chmod 700 $pfad
+fi
+
 cd $pfad
 
-public_keys=`ls *.pub`
+public_keys=$(ls *.pub 2> /dev/null)
 if [ ! -z "$public_keys" ]; then
+  nr=1
   echo -n "" > $private_key_list
   for public_key in $public_keys; do
     private_key=`basename $public_key .pub`
     if [ -f $private_key ]; then
-      echo "added $private_key"
-      echo -n "$private_key " >> $private_key_list
+      if [ $nr -gt 1 ]; then
+        echo -n " " >> $private_key_list
+	echo -n ","
+      else
+        echo -n "added "
+      fi
+      echo -n "$private_key"
+      echo -n "$private_key" >> $private_key_list
+      nr=$(($nr+1))
     fi
   done
   echo "" >> $private_key_list
+  echo ""
+  echo "added $(($nr-1)) private keys."
 else
   echo "no public keys found."
 fi
