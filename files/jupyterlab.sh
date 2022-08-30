bb4a234aa17e1d943cea93e6d228e0ca80cc6b8e
Leander Jedamus <ljedamus@web.de>
2022-08-30 15:06:31 +0200

    added set -x for debugging

--- files/jupyterlab.sh
+++ files/jupyterlab.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 # erzeugt Sonntag, 22. November 2020 09:43 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 30. August 2022 15:04 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 14:30 von Leander Jedamus
 # modifiziert Sonntag, 22. November 2020 09:43 von Leander Jedamus
+
+# set -x
 
 d=`pwd`"/env/bin"
 f="$d/activate"
