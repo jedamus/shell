c9c85c0dbc70771eeadc03d4050b25749a8507de
Leander Jedamus <ljedamus@web.de>
2022-09-05 10:31:47 +0200

    added .ipython-dir-support

--- files/jupyterlab.sh
+++ files/jupyterlab.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env sh
 
 # erzeugt Sonntag, 22. November 2020 09:43 (C) 2020 von Leander Jedamus
-# modifiziert Dienstag, 30. August 2022 15:04 von Leander Jedamus
+# modifiziert Montag, 05. September 2022 10:22 von Leander Jedamus
+# modifiziert Dienstag, 30. August 2022 11:59 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 14:30 von Leander Jedamus
 # modifiziert Sonntag, 22. November 2020 09:43 von Leander Jedamus
 
-# set -x
+#set -x
 
-d=`pwd`"/env/bin"
-f="$d/activate"
+d=`pwd`
+denv="$d/env/bin"
+dipython="$d/.ipython"
+f="$denv/activate"
+
 if [ -f "$f" ]; then
   . "$f"
   #export PATH=$d:${PATH}
   #echo $PATH
+fi
+
+if [ -d "$dipython" ]; then
+  export IPYTHONDIR="$dipython"
 fi
 
 #jupyter-notebook --browser="firefox"
