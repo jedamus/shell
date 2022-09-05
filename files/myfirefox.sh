88bb57062b8bacd3ebd8d0d757fe063175418b5b
Leander Jedamus <jedamus@users.noreply.github.com>
2022-09-05 15:57:29 +0200

    changed for better readability
    You have to CHANGE this file to reflect your firefox profiles.

--- files/myfirefox.sh
+++ files/myfirefox.sh
@@ -3,7 +3,10 @@
 # erzeugt Montag, 05. September 2022 13:57 (C) 2022 von Leander Jedamus
 # modifiziert Montag, 05. September 2022 14:09 von Leander Jedamus
 
-firefox --profile ~/.mozilla/firefox/zqx0o56g.Netflix $*
+PROFILEPATH="$HOME/.mozilla/firefox"
+PROFILE="zqx0o56g.Netflix"
+
+firefox --profile $PROFILEPATH/$PROFILE $*
 #firefox $*
 
 # vim:ai sw=2
