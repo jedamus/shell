6439a613f56e4b15b3268d90cb7c1eb82b458782
Leander Jedamus <ljedamus@web.de>
2024-03-21 13:46:19 +0100

    fixed a bug.

--- files/rep.sh
+++ files/rep.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 07. September 2020 12:59 (C) 2020 von Leander Jedamus
+# modifiziert Donnerstag, 21. März 2024 13:38 von Leander Jedamus
 # modifiziert Donnerstag, 24. September 2020 11:10 von Leander Jedamus
 # modifiziert Montag, 07. September 2020 12:59 von Leander Jedamus
 
@@ -37,7 +38,7 @@ while [ ! -z $1 ]; do
   esac
 done
 
-while /bin/true; do
+while [ -z ]; do
   $clear
   $befehl
   sleep $sleep
