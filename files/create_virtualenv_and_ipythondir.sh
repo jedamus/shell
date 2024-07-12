cc36d2a62bc0e746e23142b56036b2ff0e040350
Leander Jedamus <ljedamus@web.de>
2024-07-12 12:43:36 +0200

    added create_virtualenv_and_ipythondir.sh

--- files/create_virtualenv_and_ipythondir.sh
+++ files/create_virtualenv_and_ipythondir.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 07. September 2022 19:56 (C) 2022 von Leander Jedamus
-# modifiziert Montag, 19. September 2022 18:56 von Leander Jedamus
+# modifiziert Mittwoch, 24. April 2024 10:26 von Leander Jedamus
 # modifiziert Mittwoch, 07. September 2022 17:36 von Leander Jedamus
 
 export PY_ENV_DIR=${PY_ENV_DIR:-env}
 
-d=`pwd`
-d_ipython="$d/.ipython"
+d=$(pwd)
+d_ipython=".ipython"
+d_p_ipython="$d/.ipython"
 
-python3 -m venv $PY_ENV_DIR
+if [ ! -d $PY_ENV_DIR ]; then
+  echo -n "creating $PY_ENV_DIR ... "
+  python3 -m venv $PY_ENV_DIR
+  echo "$PY_ENV_DIR created."
+else
+  echo "$PY_ENV_DIR already exists."
+fi
 
-mkdir -p $d_ipython
-
-export IPYTHONDIR="$d_ipython"
-ipython profile create default
+if [ ! -d $d_p_ipython ]; then
+  export IPYTHONDIR="$d_p_ipython"
+  echo "creating $d_ipython ... "
+  mkdir -p $d_ipython
+  ipython profile create default
+  echo "$d_ipython created."
+else
+  echo "$d_ipython already exists."
+fi
 
 # vim:ai sw=2
-
